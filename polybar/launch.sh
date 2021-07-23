b69db7c3d2ccddbc3290382b3c69c87368281222
Hinaka Yuuki <thedatahord@yahoo.co.jp>
2021-07-23 11:38:56 +0300

    fix

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,12 +9,17 @@
 # Terminate already running bar instances
 killall -q polybar
 
+sleep 1;
+
+polybar one &
+polybar two &
+polybar three &
+
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 desktop=$(echo $DESKTOP_SESSION)
 count=$(xrandr --query | grep " connected" | cut -d" " -f1 | wc -l)
-
 
 case $desktop in
     bspwm|/usr/share/xsessions/bspwm)
@@ -25,13 +30,5 @@ case $desktop in
     else
     polybar --reload mainbar-bspwm -c ~/.config/polybar/config &
     fi
-    # second polybar at bottom
-    # if type "xrandr" > /dev/null; then
-    #   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    #     MONITOR=$m polybar --reload mainbar-bspwm-extra -c ~/.config/polybar/config &
-    #   done
-    # else
-    # polybar --reload mainbar-bspwm-extra -c ~/.config/polybar/config &
-    # fi
     ;;
 esac
