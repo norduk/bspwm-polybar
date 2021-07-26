68f948d4ac094cc0d03c1acb4a1222c54306a540
Hinaka Yuuki <thedatahord@yahoo.co.jp>
2021-07-26 10:10:57 +0300

    fix install

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ echo "Copying configurations..."
 # bspwm config
 cp -r $PWD/.config/bspwm/ $HOME/.config/bspwm/
 
-#sxhkd config
-cp -r $PWD/.config/sxhkd/sxhkdrc $HOME/.config/sxhkd/sxhkdrc
+# sxhkd config
+cp -r $PWD/.config/bspwm/sxhkd/sxhkdrc $HOME/.config/bspwm/sxhkd/sxhkdrc
 
 # polybar config
 cp -r $PWD/.config/polybar/* $HOME/.config/polybar
