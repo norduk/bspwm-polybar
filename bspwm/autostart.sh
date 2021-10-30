765f4517987402d646ca800c1deaeff0ac101933
Valera <thedatahord@yahoo.co.jp>
2021-10-30 18:31:28 +0300

    Update (notebook)

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -8,11 +8,11 @@ function run {
 }
 
 #Find out your monitor name with xrandr or arandr (save and you get this line)
-xrandr --output DP1 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI2 --primary  --mode 1920x1080 --pos 1080x536 --rotate normal --output VIRTUAL1 --off
+#xrandr --output DP1 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI2 --primary  --mode 1920x1080 --pos 1080x536 --rotate normal --output VIRTUAL1 --off
 #xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
 #xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
 #xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
-#xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 #autorandr horizontal
 
 $HOME/.config/polybar/launch.sh &
